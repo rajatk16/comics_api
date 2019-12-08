b78a60296ba9d4c78eb1d9df02b5196f7422d0d7
Rajat <rajat.sudagade@gmail.com>
2019-12-08 13:24:44 -0600

    use morgan middleware

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv').config({
   path: './config/config.env'
-})
+});
+const morgan = require('morgan');
 
-// Routes: Comics, Heroes, Auth, Users
+const comics = require('./routes/comics');
+
 const app = express();
 
-const comics = require('./routes/comics')
-
-app.use('/api/v1/comics', comics)
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use('/api/v1/comics', comics);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} and listening at ${PORT}`)
-})
+  console.log(
+    `Server running in ${process.env.NODE_ENV} and listening at ${PORT}`
+  );
+});
