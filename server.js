457ed2a8821d33f4c62c62559616c19da4112764
Rajat <rajat.sudagade@gmail.com>
2019-12-08 12:05:24 -0600

    Placeholder routes for Comics

--- server.js
+++ server.js
@@ -3,6 +3,46 @@ const dotenv = require('dotenv').config({
   path: './config/config.env'
 })
 
+// Routes: Comics, Heroes, Auth, Users
 const app = express();
+
+app.get('/api/v1/comics', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all comics'
+  });
+})
+
+app.get('/api/v1/comics/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show comic ${req.params.id}`
+  })
+})
+
+app.post('/api/v1/comics', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Add a new comic`
+  });
+})
+
+app.put('/api/v1/comics/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update comic ${req.params.id}`
+  })
+})
+
+app.delete('/api/v1/comics/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete comic ${req.params.id}`
+  })
+})
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} and listening at ${PORT}`))
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} and listening at ${PORT}`)
+})
