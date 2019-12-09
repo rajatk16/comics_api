bc33c60610285257d3899090d17c4c6719939b51
Rajat <rajat.sudagade@gmail.com>
2019-12-08 22:42:10 -0600

    Create Comic Schema

---
+++ models/Comics.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const ComicSchema = new mongoose.Schema({
+  cover: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+  price: {
+    type: String,
+    required: [true, "Please enter a valid price"],
+    default: "$2.99",
+  },
+  series: {
+    type: String
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter a small description of the comic"]
+  },
+  heroes: {
+    type: [String],
+    required: [true, "Please enter a hero name"],
+  },
+  releaseDate: {
+    type: Date,
+    required: true
+  },
+  pages: {
+    type: Number
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+  }
+})
+
+module.exports = mongoose.model("Comic", ComicSchema)
